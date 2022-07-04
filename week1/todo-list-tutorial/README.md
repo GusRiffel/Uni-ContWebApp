7b9a44aa2cd51193ae11d4b916644298e0193f60
GusRiffel <5riffg16@solent.ac.uk>
2022-07-04 16:13:38 +0100

    first commit

---
+++ week1/todo-list-tutorial/README.md
@@ -0,0 +1,12 @@
+
+# A To Do List Tutorial 
+
+Simple! Complete the to do list
+
+## To get started 
+
+- Clone this repository
+- `npm install` to install the dependencies 
+- `npm run dev-server`
+
+- Complete the todolist
---
+++ week1/todo-list-tutorial/js/main.js
@@ -0,0 +1,11 @@
+import "../main.scss";
+import img1 from "../assets/top-left-elips.png";
+import img2 from "../assets/bottom-right-elips.png";
+import todos from "./todos";
+console.log(todos);
+
+const  render = () => {
+    console.log("ready");
+}
+
+window.addEventListener('DOMContentLoaded',render);
