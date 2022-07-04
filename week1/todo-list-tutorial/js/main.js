860ffaff9a7f83fc05a149345db0ba0717c1c308
Gustavo Riffel <5riffg16@solent.ac.uk>
2022-07-04 19:05:41 +0100

    Revert "added squareNumber.js function"
    This reverts commit d1f6abdb2062f7c071a9055d89ad3b3865e07cb4.

--- week1/todo-list-tutorial/js/main.js
+++ week1/todo-list-tutorial/js/main.js
@@ -5,33 +5,10 @@ import todos from "./todos";
 import squareNumber from "./squareNumber";
 console.log(todos);
 
-window.addEventListener('DOMContentLoaded',render);
 const  render = () => {
     console.log("ready");
-
 }
 
 console.log(`The square number of 36 is ${squareNumber(36)}`);
 
-const view = ` <img
-src="${img1}"
-id="background-left"
-alt="background"
-/>
-<img
-src="${img2}"
-id="background-right"
-alt="background"
-/>
-
-<div class="wrapper">
-<div class="todolist">
-  <h1>WEB DESIGN STUDENT TO DO LIST</h1>
-  <input type="text" id="todoInput" placeholder="What do you need to do today...." />
-  <div class="list">
-  </div>
-  <!-- /.list -->
-</div>
-<!-- /.todolist -->
-</div>
-<!-- /.wrapper -->`;
+window.addEventListener('DOMContentLoaded',render);
