315ac5f722ec75e761fdcd22e7318eb4f4fb1a92
Gustavo Riffel <5riffg16@solent.ac.uk>
2022-09-13 21:12:45 +0100

    Implemented countdown

--- 5riffg16-cwa-ae2/src/components/Timer/Timer.js
+++ 5riffg16-cwa-ae2/src/components/Timer/Timer.js
@@ -1,26 +1,65 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 function Timer() {
-    let [time, setTime] = useState(5);
-    let [start, setStart] = useState(false);
+  let [time, setTime] = useState("22:00");
+  let [start, setStart] = useState(false);
 
-    useEffect(() => {
-        if (start) {
-            let interval = setInterval(() => {
-                setTime(prevTime => prevTime - 1)
-            }, 1000);
-        }
-    }, [start]);
+  useEffect(() => {
+    let interval = null;
+    if (start) {
+      let timeToSplit = time;
+      let timeSplitted = timeToSplit.split(":");
+      let timeToCalc = {
+        minutes: parseInt(timeSplitted[0]),
+        seconds: parseInt(timeSplitted[1]),
+      };
+      if (timeToCalc.seconds === 0) {
+        timeToCalc.minutes = timeToCalc.minutes - 1;
+        timeToCalc.seconds = 60;
+        interval = setInterval(() => {
+          setTimeFormatted(timeToCalc.minutes, timeToCalc.seconds - 1);
+        }, 1000);
+      } else {
+        interval = setInterval(() => {
+          setTimeFormatted(timeToCalc.minutes, timeToCalc.seconds - 1);
+        }, 1000);
+      }
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [start, time]);
 
+  const setTimeFormatted = (min, sec) => {
+    let timeToBeFormatted = {
+      minutes: min,
+      seconds: sec ? sec : 0,
+    };
+    let timeFormatted = formatTime(timeToBeFormatted);
+    if (timeToBeFormatted.minutes >= 0) {
+      setTime(timeFormatted);
+    }
+    return timeFormatted;
+  };
+
+  const formatTime = (timeObject) => {
     return (
-        <div>
-            <p>{time}</p>
-            <button onClick={() => {
-                setStart(true)
-            }}>Start
-            </button>
-        </div>
+      (timeObject.minutes > 9 ? timeObject.minutes : "0" + timeObject.minutes) +
+      ":" +
+      (timeObject.seconds > 9 ? timeObject.seconds : "0" + timeObject.seconds)
     );
+  };
+
+  return (
+    <div>
+      <button onClick={() => setTimeFormatted(25)}>Pomodoro</button>
+      <button onClick={() => setTime(5)}>Short Break</button>
+      <button onClick={() => setTime(15)}>Long Break</button>
+      <h1>{time}</h1>
+      <button onClick={() => setStart(true)}>Start</button>
+      <button onClick={() => setStart(false)}>Stop</button>
+    </div>
+  );
 }
 
 export default Timer;
