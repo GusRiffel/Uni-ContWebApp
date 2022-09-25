610c3591a2c286bab20b39dcfa5c2fdeead88222
Gustavo Riffel <5riffg16@solent.ac.uk>
2022-09-25 18:52:14 +0100

    Cleans project structure

--- 5riffg16-cwa-ae2/src/App.js
+++ 5riffg16-cwa-ae2/src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 
-import "./App.css";
 import Header from "./components/Header/index";
 import About from "./views/About";
 import Pomodoro from "./views/Pomodoro";
